5d0db09d989d55cce5cbcb2a808265fb37143690
Michael <mikhail.polikarpov@metalab.ifmo.ru>
2026-03-17 03:12:17 +0300

    убрал test из игнора

---
+++ test.py
@@ -0,0 +1,9 @@
+import Global_optimizer as glo
+
+if __name__ == "__main__":
+    optp = glo.OptimizationParameters()
+    de_p = glo.DifferentialEvolutionParameters()
+    de_p.maxiter = 2
+    optimizer = glo.DifferentialEvolutionOptimizer(optp, de_p, result_path="test_de.json", detailed=True, is_single_segment=True)
+    res = optimizer.optimize()
+    print(optp.segment_points)
