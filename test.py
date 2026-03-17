922fbfc09b759a48f76429232e1bf0fd8f7519f0
Michael <mikhail.polikarpov@metalab.ifmo.ru>
2026-03-17 12:53:50 +0300

    убрал test из игнора

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ import Global_optimizer as glo
 if __name__ == "__main__":
     optp = glo.OptimizationParameters()
     de_p = glo.DifferentialEvolutionParameters()
-    de_p.maxiter = 2
+    de_p.maxiter = 160
+    de_p.popsize = 40
     optimizer = glo.DifferentialEvolutionOptimizer(optp, de_p, result_path="kaka/test_de.json", detailed=True, is_single_segment=True)
     res = optimizer.optimize()
     print(optp.segment_points)
