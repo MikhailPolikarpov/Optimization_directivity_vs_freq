82ec9f40175ae8d0bc7f72d3297888616416343d
Michael <mikhail.polikarpov@metalab.ifmo.ru>
2026-03-17 12:51:19 +0300

    эксперимент с seed

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import Global_optimizer as glo
 
 if __name__ == "__main__":
-    optp = glo.OptimizationParameters()
-    de_p = glo.DifferentialEvolutionParameters()
-    de_p.maxiter = 2
-    optimizer = glo.DifferentialEvolutionOptimizer(optp, de_p, result_path="kaka/test_de.json", detailed=True, is_single_segment=True)
-    res = optimizer.optimize()
-    print(optp.segment_points)
+    for seed in [2, 43, 90, 5]:
+        optp = glo.OptimizationParameters()
+        de_p = glo.DifferentialEvolutionParameters()
+        de_p.maxiter = 80
+        de_p.popsize = 15
+        de_p.seed = seed
+        optimizer = glo.DifferentialEvolutionOptimizer(optp, de_p, result_path=f"different_seeds/test_de_seed_{seed}.json", detailed=True, is_single_segment=True)
+        res = optimizer.optimize()
