0f113053380719b302ca0d157f72a190f0e65c13
Michael Polikarpov <mikhail.polikarpov@metalab.ifmo.ru>
2026-05-04 21:27:44 +0300

    resonator freq

--- test.py
+++ test.py
@@ -1,21 +1,23 @@
 import Global_optimizer as glo
+import numpy as np
 
 if __name__ == "__main__":
-    for seed in [90]:
-        thres = [19]
-        widths = [30]
-        for i in range(len(thres)):
-            optp = glo.OptimizationParameters()
-            optp.n = 3
-            optp.bounds_alpha = [(40, 0), (-30.0, 0), (30.0, 0)]
-            optp.bounds_beta = [(0.05, 7.0)] + [(0.05, 4.0)]*2
-            optp.eps = 1e-3
-            optp.limit = 200
-            optp.sigma_thres = thres[i] - 0.5
-            optp.segment_width_percent = widths[i]
-            de_p = glo.DifferentialEvolutionParameters()
-            de_p.maxiter = 200
-            de_p.popsize = 16
-            de_p.seed = seed
-            optimizer = glo.DifferentialEvolutionOptimizer(optp, de_p, result_path=f"results/test/width{widths[i]}de2_{thres[i]}_seed_{seed}.json", detailed=False)
-            res = optimizer.optimize()
+    for n in [1]:
+        for seed in [90]:
+            thres = [14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 21.0, 22.0]
+            widths = np.array([45.0, 40.0, 35.0, 30.0, 27.0, 25.0, 23.0, 20.0, 18.0])*(1+(n-3)*0.3)
+            for i in range(len(thres)):
+                optp = glo.OptimizationParameters()
+                optp.n = n
+                optp.bounds_alpha = [(50.0, 0)] + [(30.0, 0)]*(n-1)
+                optp.bounds_beta = [(0.1, 3.0)] + [(0.1, 3.0)]*(n-1)
+                optp.eps = 1e-3
+                optp.limit = 200
+                optp.sigma_thres = thres[i] - 0.5
+                optp.segment_width_percent = widths[i]
+                de_p = glo.DifferentialEvolutionParameters()
+                de_p.maxiter = 200
+                de_p.popsize = 32
+                de_p.seed = seed
+                optimizer = glo.DifferentialEvolutionOptimizer(optp, de_p, result_path=f"kz/{n}sh/de_{thres[i]}_seed_{seed}.json", detailed=False)
+                res = optimizer.optimize()
